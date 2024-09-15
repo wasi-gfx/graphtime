34685b034de0ec3d3470da50df610f814de3e08b
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-09-15 11:16:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Temporary wasm runtime with wasi and wasi-webgpu support
+This runtime will be deprecated once more popular runtimes add wasi-webgpu support.
+
+
+#### Usage:
+```shell
+cargo run [path to wasm component]
+```
---
+++ src/main.rs
@@ -0,0 +1,132 @@
+use anyhow::Context;
+use clap::Parser;
+use std::sync::Arc;
+use wasi_frame_buffer_wasmtime::WasiFrameBufferView;
+use wasi_graphics_context_wasmtime::WasiGraphicsContextView;
+use wasi_mini_canvas_wasmtime::{MiniCanvas, MiniCanvasDesc, WasiMiniCanvasView};
+use wasi_webgpu_wasmtime::WasiWebGpuView;
+use wasmtime::{
+    component::{Component, Linker},
+    Config, Engine, Store,
+};
+
+use wasmtime_wasi::{ResourceTable, WasiCtx, WasiCtxBuilder, WasiView};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    file: String,
+}
+
+struct HostState {
+    pub table: ResourceTable,
+    pub ctx: WasiCtx,
+    pub instance: Arc<wasi_webgpu_wasmtime::reexports::wgpu_core::global::Global>,
+    pub main_thread_proxy: wasi_mini_canvas_wasmtime::WasiWinitEventLoopProxy,
+}
+
+impl HostState {
+    fn new(main_thread_proxy: wasi_mini_canvas_wasmtime::WasiWinitEventLoopProxy) -> Self {
+        Self {
+            table: ResourceTable::new(),
+            ctx: WasiCtxBuilder::new().inherit_stdio().build(),
+            instance: Arc::new(wasi_webgpu_wasmtime::reexports::wgpu_core::global::Global::new(
+                "webgpu",
+                wasi_webgpu_wasmtime::reexports::wgpu_types::InstanceDescriptor {
+                    backends: wasi_webgpu_wasmtime::reexports::wgpu_types::Backends::all(),
+                    flags: wasi_webgpu_wasmtime::reexports::wgpu_types::InstanceFlags::from_build_config(),
+                    dx12_shader_compiler: wasi_webgpu_wasmtime::reexports::wgpu_types::Dx12Compiler::Fxc,
+                    gles_minor_version: wasi_webgpu_wasmtime::reexports::wgpu_types::Gles3MinorVersion::default(),
+                },
+            )),
+            main_thread_proxy,
+        }
+    }
+}
+
+impl WasiView for HostState {
+    fn table(&mut self) -> &mut ResourceTable {
+        &mut self.table
+    }
+
+    fn ctx(&mut self) -> &mut WasiCtx {
+        &mut self.ctx
+    }
+}
+
+impl WasiGraphicsContextView for HostState {}
+impl WasiFrameBufferView for HostState {}
+
+struct UiThreadSpawner(wasi_mini_canvas_wasmtime::WasiWinitEventLoopProxy);
+
+impl wasi_webgpu_wasmtime::MainThreadSpawner for UiThreadSpawner {
+    async fn spawn<F, T>(&self, f: F) -> T
+    where
+        F: FnOnce() -> T + Send + Sync + 'static,
+        T: Send + Sync + 'static,
+    {
+        self.0.spawn(f).await
+    }
+}
+
+impl WasiWebGpuView for HostState {
+    fn instance(&self) -> Arc<wasi_webgpu_wasmtime::reexports::wgpu_core::global::Global> {
+        Arc::clone(&self.instance)
+    }
+
+    fn ui_thread_spawner(&self) -> Box<impl wasi_webgpu_wasmtime::MainThreadSpawner + 'static> {
+        Box::new(UiThreadSpawner(self.main_thread_proxy.clone()))
+    }
+}
+
+impl WasiMiniCanvasView for HostState {
+    fn create_canvas(&self, desc: MiniCanvasDesc) -> MiniCanvas {
+        pollster::block_on(self.main_thread_proxy.create_window(desc))
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // env_logger::builder()
+    //     .filter_level(log::LevelFilter::Info)
+    //     .init();
+
+    let args = Args::parse();
+
+    let mut config = Config::default();
+    config.wasm_component_model(true);
+    config.async_support(true);
+    let engine = Engine::new(&config)?;
+    let mut linker: Linker<HostState> = Linker::new(&engine);
+
+    wasi_webgpu_wasmtime::add_to_linker(&mut linker)?;
+    wasi_frame_buffer_wasmtime::add_to_linker(&mut linker)?;
+    wasi_graphics_context_wasmtime::add_to_linker(&mut linker)?;
+    wasi_mini_canvas_wasmtime::add_only_mini_canvas_to_linker(&mut linker)?;
+    wasmtime_wasi::add_to_linker_sync(&mut linker)?;
+
+    let (main_thread_loop, main_thread_proxy) =
+        wasi_mini_canvas_wasmtime::create_wasi_winit_event_loop();
+    let host_state = HostState::new(main_thread_proxy);
+
+    let mut store = Store::new(&engine, host_state);
+
+    let component =
+        Component::from_file(&engine, &args.file).context("Component file not found")?;
+
+    let (command, _instance) =
+        wasmtime_wasi::bindings::Command::instantiate_async(&mut store, &component, &linker)
+            .await
+            .unwrap();
+
+    std::thread::spawn(move || {
+        pollster::block_on(command.wasi_cli_run().call_run(&mut store))
+            .context("failed to invoke `run` function")
+            .unwrap()
+            .unwrap();
+    });
+
+    main_thread_loop.run();
+
+    Ok(())
+}
